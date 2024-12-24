87a541df2e225e5408aa8ddfbf189573c2856eac
Dongwha <dreamtac@gmail.com>
2024-12-24 23:12:25 +0900

    feat: next-mdx-remote -> next/mdx 변경

---
+++ dev_blog/lib/post.ts
@@ -0,0 +1,37 @@
+// import fs from 'fs';
+// import path from 'path';
+// import matter from 'gray-matter';
+// import readingTime from 'reading-time';
+// import dayjs from 'dayjs';
+
+// const BASE_PATH = '../posts';
+// const POSTS_PATH = path.join(process.cwd(), BASE_PATH);
+
+// // Mdx 파싱: abstract / detail 구분
+// const parsePost = async (postPath: string): Promise<Post> => {
+//     const postAbstract = parsePostAbstract(postPath);
+//     const postDetail = await parsePostDetail(postPath);
+//     return { ...postAbstract, ...postDetail };
+//   };
+
+// // mdx Abstract
+// export const parsePostAbstract = (postPath: string) => {
+//     const filePath = postPath.slice(postPath.indexOf(BASE_PATH)).replace(`${BASE_PATH}/`, '').replace('.mdx', '');
+
+//     const [category, slug] = filePath.split('/');
+
+//     const url = `/blog/${category}/${slug}`;
+
+//     return { url, category, slug };
+// };
+
+// // mdx Detail
+// const parsePostDetail = async (postPath: string)=>{
+//     const file = fs.readFileSync(postPath, 'utf-8');
+//     const {data, content} = matter(file);
+//     const grayMatter = data as PostMatter;
+//     const readingMinutes = Math.ceil(readingTime(content).minutes);
+//     const dateString = dayjs(grayMatter.date).locale('ko').format('YYYY년 MM월 DD일');
+//     return {...grayMatter, dateString, content, readingMinutes};
+
+// }
