754187857d3070fe90d42c9cdc19f277530807e5
Dongwha <dreamtac@gmail.com>
2024-12-25 23:08:20 +0900

    feat: next/mdx 세팅

--- dev_blog/app/blog/page.tsx
+++ dev_blog/app/blog/page.tsx
@@ -1,9 +1,5 @@
 // import NavBar from '@/components/nav_bar';
 import { redirect } from 'next/navigation';
 export default function BlogHome() {
-    return redirect('/blog/All');
-    // <div className="flex flex-col">
-    //     <NavBar />
-    //     Blog Home
-    // </div>
+    return redirect('/blog/all');
 }
--- dev_blog/next.config.ts
+++ dev_blog/next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import nextMDX from '@next/mdx';
+import type { NextConfig } from 'next';
+
+const withMDX = nextMDX({
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [],
+        rehypePlugins: [],
+    },
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    reactStrictMode: true,
+    /* config options here */
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);
