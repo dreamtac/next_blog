b9a1d8eb8fa539a951a0ded2e36762b08a3ed755
Dongwha <dreamtac@gmail.com>
2024-12-26 20:49:56 +0900

    feat: Post list 테스트

--- dev_blog/app/blog/page.tsx
+++ dev_blog/app/blog/page.tsx
@@ -1,11 +1,14 @@
 import NavBar from '@/components/nav_bar';
-import First from '../../posts/coding/first/page.mdx';
+import { PostList } from '@/components/post_list';
+import { getPosts } from '@/lib/post';
 
-export default function BlogHome() {
+export default async function BlogHome() {
+    const posts = await getPosts();
     return (
         <div>
             <NavBar categoryParam="" />
-            <First />
+            {/* <First /> */}
+            <PostList posts={posts} />
         </div>
     );
     // return redirect('/blog/all');
--- dev_blog/lib/post.ts
+++ dev_blog/lib/post.ts
@@ -46,18 +46,32 @@ interface Post {
 }
 
 export async function getPosts(): Promise<Post[]> {
-    const postPath = path.resolve(process.cwd(), 'src', 'app', 'blog', '(posts)');
-
-    const slugs = (await readdir(postPath, { withFileTypes: true })).filter(dirent => dirent.isDirectory());
+    const postDir = path.join(process.cwd(), 'app', '(posts)');
+    const slugs = (await readdir(postDir, { withFileTypes: true })).filter(dirent => dirent.isDirectory());
 
     const posts = await Promise.all(
         slugs.map(async ({ name }) => {
-            const { metadata } = await import(`../app/blog/(posts)/${name}/page.mdx`);
+            const { metadata } = await import(`../app/(posts)/${name}/page.mdx`);
             return { slug: name, ...metadata };
         })
     );
 
     posts.sort((a, b) => +new Date(b.publishDate) - +new Date(a.publishDate));
-
     return posts;
 }
+
+// export async function getPosts(): Promise<Post[]> {
+//     const postPath = path.resolve(process.cwd(), 'src', 'app', 'blog', '(posts)');
+//     const slugs = (await readdir(postPath, { withFileTypes: true })).filter(dirent => dirent.isDirectory());
+
+//     const posts = await Promise.all(
+//         slugs.map(async ({ name }) => {
+//             const { metadata } = await import(`../app/blog/(posts)/${name}/page.mdx`);
+//             return { slug: name, ...metadata };
+//         })
+//     );
+
+//     posts.sort((a, b) => +new Date(b.publishDate) - +new Date(a.publishDate));
+
+//     return posts;
+// }
