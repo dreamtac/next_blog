736e699e3df3ce1fb857d017e0da4ab12c79dd9a
Dongwha <dreamtac@gmail.com>
2025-01-03 13:50:31 +0900

    feat: Add rehype plugin
    rehype pretty code 추가, turbopack 제거, 코드블럭 css

--- dev_blog/next.config.ts
+++ dev_blog/next.config.ts
@@ -1,18 +1,26 @@
 import nextMDX from '@next/mdx';
 import type { NextConfig } from 'next';
+import rehypePrettyCode from 'rehype-pretty-code';
+
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+    // See Options section below.
+    keepBackground: false,
+    theme: 'rose-pine',
+};
 
 const withMDX = nextMDX({
     extension: /\.mdx?$/,
     options: {
         remarkPlugins: [],
-        rehypePlugins: [],
+        rehypePlugins: [[rehypePrettyCode, options]],
     },
 });
 
 const nextConfig: NextConfig = {
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     reactStrictMode: true,
-    /* config options here */
+    /* 추가 설정 가능 */
 };
 
 export default withMDX(nextConfig);
