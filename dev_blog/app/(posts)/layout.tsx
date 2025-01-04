a0f0bff8fb901940ab912c27bef8f10ac9b701c4
Dongwha <dreamtac@gmail.com>
2025-01-04 12:56:47 +0900

    design: mdx layout 수정

--- dev_blog/app/(posts)/layout.tsx
+++ dev_blog/app/(posts)/layout.tsx
@@ -1,7 +1,7 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex justify-center min-h-screen">
-            <div className="prose max-w-3xl">{children}</div>
+            <div className="prose max-w-3xl w-full px-4">{children}</div>
         </div>
     );
 }
