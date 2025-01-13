dcad3d0ebf937cc7cc454bd378abd0eb3a84ef56
Dongwha <dreamtac@gmail.com>
2025-01-13 23:20:34 +0900

    feat: 파라미터 유효성 검사

--- dev_blog/app/blog/[category]/page.tsx
+++ dev_blog/app/blog/[category]/page.tsx
@@ -1,9 +1,14 @@
 import NavBar from '@/components/nav_bar';
 import { PostList } from '@/components/post_list';
 import { getPosts } from '@/lib/post';
+import { notFound } from 'next/navigation';
 export default async function CategoryHome({ params }: { params: { category: string } }) {
     const { category } = await params;
     const posts = await getPosts(category);
+    const validCategories = ['all', 'coding', 'daily', 'projects', 'etc'];
+    if (!validCategories.includes(category)) {
+        return notFound();
+    }
     return (
         <div>
             <NavBar categoryParam={`${category}`} />
